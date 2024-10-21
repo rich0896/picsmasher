52f99e7ff046f39d934217aeae260b4a64949c73
rich0896 <rich0896@algonquinlive.com>
2024-10-21 01:07:33 -0400

    Fixes incorrect build directory for deployment

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = (env, argv) => ({
     entry: './src/script.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, '/dist'),
+        path: path.resolve(__dirname, 'dist'),
         clean: true,
     },
     mode: argv.mode || 'development',
